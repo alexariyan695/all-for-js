f496c5f526f2357df7fbc362a381ad185aaa4bac
alexariyan695 <alexariyanxhowdhury695@gmail.com>
2025-05-20 17:36:56 +0600

    upload

---
+++ isfound.js
@@ -0,0 +1,31 @@
+// var arr=[20,10,50,45,70,100,50]
+
+
+// var find=20
+
+// isFound=false
+
+// for(var i =0; i<arr.length;i++){
+//     if(arr[i]===find){
+//         console.log('Date is found inex ' + i)
+//         isFound=true
+//         break;
+//     }
+// }
+
+
+// if(!isFound){
+//     console.log('date not found ')
+// }
+
+
+var arr2=[10,11,20,21,45,70,80,]
+
+
+for(var i=0;i<arr2.length;i++){
+    if(arr2[i]%2===0){
+        console.log(i +' Is even number')
+    }else{
+        console.log(i + ' Is odd number')
+    }
+}
